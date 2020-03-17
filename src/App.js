edfdf3ef3e542bba9447bf7a6a225fee678effe9
shivam bale <b.shivam@novasemita.org>
2020-03-18 00:51:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import CountrySelector from './components/CountrySelector';
 function App() {
   return (
     <div >
-      <h1 className="main">Corona Virus States World-wide and Country wise</h1>
-      <h1 className="heading">Showing World Stats</h1>
+      <h1 className="main">COVID-19 CORONAVIRUS OUTBREAK</h1>
+      <h1 className="heading">Showing World-wide Statistics</h1>
       <Stats url="https://covid19.mathdro.id/api"></Stats>
       <CountrySelector></CountrySelector>
     </div>
